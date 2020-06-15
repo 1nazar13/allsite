7d53e43888694d17ad9a0ba710d1cef8ae7a5179
1nazar13 <ukraine13712@gmail.com>
2020-06-15 17:55:49 +0300

    s

---
+++ index.js
@@ -0,0 +1,36 @@
+(function(){
+    const days = document.getElementById("days");
+    const hours = document.getElementById("hours");
+    const minutes = document.getElementById("minutes");
+    const seconds = document.getElementById("seconds");
+  
+    const currentDate = new Date().getFullYear();
+   
+    let concertDate = new Date(`June 17 ${currentDate} 00:00:00`).getTime();
+   
+    function updateCountdown() {
+    const currentTime = new Date();
+    const diff = concertDate - currentTime;
+   
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+   
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? "0" + h : h;
+    minutes.innerHTML = m < 10 ? "0" + m : m;
+    seconds.innerHTML = s < 10 ? "0" + s : s;
+   
+  
+     }
+   
+   setInterval(updateCountdown, 1000);
+   
+   })();
+
+  
+  
+  
+  
+  
